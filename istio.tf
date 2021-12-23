f938520ddb2626cb3cf02de256fe4b28a71dcd59
Talha-Altair <cooltalha27@gmail.com>
2021-12-23 16:34:40 +0530

    Updates for 2021-12-23 16:34:40

--- istio.tf
+++ istio.tf
@@ -1,10 +1,5 @@
-resource "kubernetes_namespace" "istio_system" {
-  metadata {
-    name = "istio-system"
-  }
-}
+resource "helm_release" "deployment" {
 
-resource "helm_release" "istio_base" {
   name  = "istio-base"
   chart = "./istio-base-charts/base"
 
@@ -12,55 +7,9 @@ resource "helm_release" "istio_base" {
   cleanup_on_fail = true
   force_update    = true
   namespace       = "istio-system"
+  create_namespace = true
 
 
   depends_on = [module.gke-cluster, kubernetes_namespace.istio_system]
-}
-
-resource "helm_release" "istiod" {
-  name  = "istiod"
-  chart = "./istio-base-charts/istio-control/istio-discovery"
-
-  timeout         = 120
-  cleanup_on_fail = true
-  force_update    = true
-  namespace       = "istio-system"
-
-  depends_on = [module.gke-cluster, kubernetes_namespace.istio_system, helm_release.istio_base]
-}
-
-resource "helm_release" "istio_ingress" {
-  name  = "istio-ingress"
-  chart = "./istio-base-charts/gateways/istio-ingress"
-
-  timeout         = 120
-  cleanup_on_fail = true
-  force_update    = true
-  namespace       = "istio-system"
-
-  depends_on = [module.gke-cluster, kubernetes_namespace.istio_system, helm_release.istiod]
-}
-
-resource "helm_release" "istio_egress" {
-  name  = "istio-egress"
-  chart = "./istio-base-charts/gateways/istio-egress"
-
-  timeout         = 120
-  cleanup_on_fail = true
-  force_update    = true
-  namespace       = "istio-system"
-
-  depends_on = [module.gke-cluster, kubernetes_namespace.istio_system, helm_release.istiod]
-}
-
-resource "helm_release" "istio_addons" {
-  name  = "istio-addons"
-  chart = "./istio-addons-chart"
-
-  timeout         = 120
-  cleanup_on_fail = true
-  force_update    = true
-  namespace       = "istio-system"
-
-  depends_on = [module.gke-cluster, kubernetes_namespace.istio_system, helm_release.istiod]
+  
 }
--- providers.tf
+++ providers.tf
@@ -1,22 +1,7 @@
-provider "google" {
-
-  project     = var.project_id
-  region      = var.region
-  credentials = file(var.creds_file_path)
-
-}
-
-data "google_client_config" "default" {}
-
 provider "helm" {
   kubernetes {
     host                   = "https://${module.gke-cluster.cluster_endpoint}"
     token                  = data.google_client_config.default.access_token
     cluster_ca_certificate = base64decode(module.gke-cluster.ca_cert)
   }
-}
-provider "kubernetes" {
-  host                   = "https://${module.gke-cluster.cluster_endpoint}"
-  token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(module.gke-cluster.ca_cert)
 }
