5c4cffa2158d94b7f8aeb28d64ac65f9ca220a5f
Talha-Altair <cooltalha27@gmail.com>
2021-12-24 16:57:09 +0530

    Updates for 2021-12-24 16:57:09

--- deployments.tf
+++ deployments.tf
@@ -1,12 +1,39 @@
-resource "helm_release" "deployment" {
+resource "helm_release" "admin" {
 
-  name  = "deployment-1"
-  chart = "./deployment-chart"
+  name  = "admin"
+  chart = "charts/admin"
 
   timeout          = 120
   cleanup_on_fail  = true
   force_update     = true
-  namespace        = "test"
+  namespace        = var.namespace
+  create_namespace = true
+
+}
+
+resource "helm_release" "ais" {
+
+  name  = "ais"
+  chart = "charts/ais"
+
+  timeout          = 120
+  cleanup_on_fail  = true
+  force_update     = true
+  namespace        = var.namespace
+  create_namespace = true
+
+}
+
+
+resource "helm_release" "ccms" {
+
+  name  = "ccms"
+  chart = "charts/ccms"
+
+  timeout          = 120
+  cleanup_on_fail  = true
+  force_update     = true
+  namespace        = var.namespace
   create_namespace = true
 
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "KUBECONFIG_PATH" {
   type        = string
   description = "Path to Kube Config file"
 }
+
+variable "namespace" {
+  type = string
+  description = "namespace to deploy to"
+}
