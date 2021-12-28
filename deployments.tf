80bd35684ee036e2077d8e04e000ca28da432c67
Talha-Altair <cooltalha27@gmail.com>
2021-12-28 16:27:47 +0530

    Updates for 2021-12-28 16:27:47

--- deployments.tf
+++ deployments.tf
@@ -1,28 +1,30 @@
-resource "helm_release" "admin" {
+# resource "helm_release" "admin" {
 
-  name  = "admin"
-  chart = "charts/admin"
+#   name  = "admin"
+#   chart = "charts/admin"
 
-  timeout          = 120
-  cleanup_on_fail  = true
-  force_update     = true
-  namespace        = var.namespace
-  create_namespace = true
+#   timeout          = 120
+#   cleanup_on_fail  = true
+#   force_update     = true
+#   namespace        = var.namespace
+#   create_namespace = true
 
-}
+# }
 
-resource "helm_release" "ais" {
+# resource "helm_release" "ais" {
 
-  name  = "ais"
-  chart = "charts/ais"
+#   name  = "ais"
+#   chart = "charts/ais"
 
-  timeout          = 120
-  cleanup_on_fail  = true
-  force_update     = true
-  namespace        = var.namespace
-  create_namespace = true
+#   timeout          = 120
+#   cleanup_on_fail  = true
+#   force_update     = true
+#   namespace        = var.namespace
+#   create_namespace = true
 
-}
+# }
+
+# Unncomment Above code to deploy the specific helm charts accodingly
 
 
 resource "helm_release" "ccms" {
